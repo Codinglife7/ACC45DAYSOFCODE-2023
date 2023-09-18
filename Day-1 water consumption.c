54b795076de74de4364c826fbb85c6ee7ade39d9
Codinglife7 <145310946+Codinglife7@users.noreply.github.com>
2023-09-18 20:40:15 +0530

    Create Day-1 water consumption.c

---
+++ Day-1 water consumption.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+
+int main(void) {
+
+	// your code goes here
+
+	int a,n,i=1;
+
+	scanf("%d",&n);
+
+	while(i<=n)
+
+	{
+
+	    scanf("%d",&a);
+
+	    if(a>=2000)
+
+	    {
+
+	        printf("YES\n");
+
+	    }
+
+	    else
+
+	    {
+
+	        printf("NO\n");
+
+	    }
+
+	    i++;
+
+	}
+
+	
+
+	return 0;
+
+}
